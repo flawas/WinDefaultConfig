83592a97309ca077fe28d518d7b181782dbb0663
Flavio Waser <32436687+flawas@users.noreply.github.com>
2023-03-02 15:16:12 +0100

    Further settings added

--- WinDefaultConfig.ps1
+++ WinDefaultConfig.ps1
@@ -12,7 +12,7 @@ if(-not ([string]::IsNullOrEmpty($Password))){
 Write-Host Hostname eingeben:
 $Hostname = Read-Host
 if(-not ([string]::IsNullOrEmpty($Hostname))){
-    # Rename-Computer -NewName $Hostname
+    Rename-Computer -NewName $Hostname
 }
 
 # Install winget
@@ -89,8 +89,8 @@ if(-Not (Test-Path C:\admin).Exists){
 }
 
 # Set Path Variable TEMP and TMP to C:\temp
-#[Environment]::SetEnvironmentVariable("TEMP", $Env:PATH + ";C:\temp", [EnvironmentVariableTarget]::Machine)
-#[Environment]::SetEnvironmentVariable("TMP", $Env:PATH + ";C:\temp", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable('TMP', 'C:\Temp', 'User')
+[Environment]::SetEnvironmentVariable('TEMP', 'C:\Temp', 'User')
 
 # Install .NET 
 Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3"
@@ -106,3 +106,105 @@ Powercfg /hibernate off
 # Set Monitor TimeOut to 0
 powercfg /change monitor-timeout-ac 0
 powercfg /change standby-timeout-dc 0
+
+# Set Explorer Settings Actual User
+$explorerkey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer'
+Set-ItemProperty "$explorerkey" EnableAutoTray 0
+Set-ItemProperty "$explorerkey\Advanced" FolderContentsInfoTip 0
+Set-ItemProperty "$explorerkey\Advanced" FolderSizeTip 0
+Set-ItemProperty "$explorerkey\Advanced" HideFileExt 0
+Set-ItemProperty "$explorerkey\Advanced" Hidden 1
+Set-ItemProperty "$explorerkey\Advanced" IconsOnly 1
+Set-ItemProperty "$explorerkey\Advanced" LaunchTo 1
+Set-ItemProperty "$explorerkey\Advanced" SharingWizardOn 0
+Set-ItemProperty "$explorerkey\Advanced" ShowInfoTip 0
+Set-ItemProperty "$explorerkey\Advanced" ShowTaskViewButton 0
+Set-ItemProperty "$explorerkey\Advanced" ShowTypeOverlay 0
+Set-ItemProperty "$explorerkey\VisualEffects" VisualFXSetting 2
+Set-ItemProperty "$explorerkey\Ribbon" MinimizedStateTabletModeOff 0
+
+$null = New-ItemProperty -Path "$explorerkey\Advanced" -Name ShowSyncProviderNotifications -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$explorerkey\Advanced" -Name NavPaneShowAllFolders -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "$explorerkey\Advanced" -Name NavPaneExpandToCurrentFolder -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "$explorerkey\CabinetState" -Name FullPath -PropertyType DWord -Value 1 -Force
+
+$null = New-Item -Path "$explorerkey\ControlPanel"
+$null = New-ItemProperty -Path "$explorerkey\ControlPanel" -Name AllItemsIconView -PropertyType DWord -Value 1 -Force
+
+$null = New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer"
+$null = New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name ExplorerRibbonStartsMinimized -PropertyType DWord -Value 2 -Force
+
+# Set Explorer SEttings Default User
+$null = New-PSDrive HKU Registry HKEY_USERS
+$null = reg load HKLM\DefaultUser C:\Users\Default\NTUSER.DAT
+
+# Set CMD QuickEdit
+Set-ItemProperty  'HKLM:\Defaultuser\Console' QuickEdit 1
+
+# Set Folder options
+$defuserexplorer = "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Explorer"
+
+$null = New-ItemProperty -Path "$defuserexplorer" -Name EnableAutoTray -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name FolderContentsInfoTip -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name FolderSizeTip -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name HideFileExt -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name Hidden -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name IconsOnly -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name LaunchTo -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name SharingWizardOn -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name ShowInfoTip -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name ShowSyncProviderNotifications -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name ShowTaskViewButton -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name ShowTypeOverlay -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name ShowSyncProviderNotifications -PropertyType DWord -Value 0 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name NavPaneShowAllFolders -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "$defuserexplorer\Advanced" -Name NavPaneExpandToCurrentFolder -PropertyType DWord -Value 1 -Force
+
+$null = New-Item -Path "$defuserexplorer\VisualEffects"
+$null = New-ItemProperty -Path "$defuserexplorer\VisualEffects" -Name VisualFXSetting -PropertyType DWord -Value 2 -Force
+$null = New-Item -Path "$defuserexplorer\CabinetState"
+$null = New-ItemProperty -Path "$defuserexplorer\CabinetState" -Name FullPath -PropertyType DWord -Value 1 -Force
+$null = New-Item -Path "$defuserexplorer\ControlPanel"
+$null = New-ItemProperty -Path "$defuserexplorer\ControlPanel" -Name AllItemsIconView -PropertyType DWord -Value 1 -Force
+
+Write-Host "Disable Input Indicator..."
+$null = New-Item -Path "HKLM:\Defaultuser\Software\Microsoft\CTF\LangBar"
+$null = New-ItemProperty -Path "HKLM:\Defaultuser\Software\Microsoft\CTF\LangBar" -Name ShowStatus -PropertyType DWord -Value 3 -Force
+
+Write-Host "System Tray Customizations..."
+if ( $win22 -eq $true ) { $null = New-Item "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Search" }
+$null = New-ItemProperty -Path "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Search" -Name SearchboxTaskbarMode -PropertyType DWord -Value 0 -Force
+$null = New-Item "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Policies"
+$null = New-Item "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$null = New-ItemProperty -Path "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name HideSCAVolume -PropertyType DWord -Value 1 -Force
+$null = New-ItemProperty -Path "HKLM:\Defaultuser\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name TaskbarNoNotification -PropertyType DWord -Value 0 -Force
+
+# Unload registry tree
+$tmpreg = Get-ChildItem -Path "Registry::HKEY_LOCAL_MACHINE\Defaultuser\"
+$tmpreg.Close()
+[gc]::collect()
+Start-Sleep -Seconds 2
+$null = reg unload HKLM\DefaultUser
+
+
+# Disable Input Indicator
+$null = New-Item -Path "HKCU:\Software\Microsoft\CTF\LangBar"
+$null = New-ItemProperty -Path "HKCU:\Software\Microsoft\CTF\LangBar" -Name ShowStatus -PropertyType DWord -Value 3 -Force
+
+# Disable Crash Dumps
+$null = bcdedit /timeout 0
+
+# Disable IE Security
+$null = New-ItemProperty -Path 'HKLM:\Software\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' -Name IsInstalled -PropertyType DWord -Value 0 -Force
+
+# Set Active Hours
+Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' ActiveHoursStart 6
+Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' ActiveHoursEnd 23
+
+# Disable IPv6
+Disable-NetAdapterBinding -InterfaceAlias "*" -ComponentID "ms_tcpip6"
+
+# Set TimeServer
+$null = w32tm /config /manualpeerlist:"ch.pool.ntp.org,0x8" /syncfromflags:manual /reliable:yes /update
+$null = net stop w32time
+$null = net start w32time
